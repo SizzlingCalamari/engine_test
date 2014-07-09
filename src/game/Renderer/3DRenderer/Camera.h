d0cd143c4873778f59470915e1f7dbcd4588af29
Jordan Cristiano <jordan.cristi@gmail.com>
2014-07-08 21:56:11 -0400

    Added position and orientation to renderer camera

--- src/game/Renderer/3DRenderer/Camera.h
+++ src/game/Renderer/3DRenderer/Camera.h
@@ -11,6 +11,9 @@ class Camera
 public:
     void CalcView(const glm::vec3& position, const glm::quat& orientation)
     {
+        m_position = position;
+        m_orientation = orientation;
+
         auto up = math::up(orientation);
         auto forward = math::forward(orientation);
 
@@ -22,6 +25,18 @@ public:
         return m_view;
     }
 
+    const glm::vec3& GetPosition() const
+    {
+        return m_position;
+    }
+
+    const glm::quat& GetOrientation() const
+    {
+        return m_orientation;
+    }
+
 private:
     glm::mat4 m_view;
+    glm::vec3 m_position;
+    glm::quat m_orientation;
 };
