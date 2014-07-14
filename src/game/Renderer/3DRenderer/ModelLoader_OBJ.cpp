4de531bf5611959068c2b74bf4d91014aa9d2a56
Jordan Cristiano <jordan.cristi@gmail.com>
2014-07-14 00:59:30 -0400

    Optimized OBJ file loading by switching from ifstream to fscanf

--- src/game/Renderer/3DRenderer/ModelLoader_OBJ.cpp
+++ src/game/Renderer/3DRenderer/ModelLoader_OBJ.cpp
@@ -1,12 +1,12 @@
 
 #include "ModelLoader_OBJ.h"
-#include <glm/glm.hpp>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
 #include <vector>
 #include <deque>
-#include <fstream>
+#include <cstdio>
 #include <string>
 #include <regex>
-#include <limits>
 
 enum FaceType
 {
@@ -40,21 +40,10 @@ FaceType identifyFace(const std::string& face)
     return Pos;
 }
 
-std::istream& operator>>(std::istream& stream, const char* str)
+inline void SkipToNextLine(FILE* file)
 {
-    const char* it = str;
-    while (*it != '\0')
-    {
-        char next;
-        stream.get(next);
-        if (next != *it)
-        {
-            stream.setstate(std::ios::failbit);
-            return stream;
-        }
-        ++it;
-    }
-    return stream;
+    char c;
+    while ((c = fgetc(file)) != EOF && c != '\n');
 }
 
 void ParseOBJ(const char *filename,
@@ -62,100 +51,108 @@ void ParseOBJ(const char *filename,
               std::vector<glm::vec2>& uvcoords,
               std::vector<glm::vec3>& normals)
 {
-    using namespace std;
-    deque<glm::vec3> verticies_temp;
-    deque<glm::vec2> uvs_temp;
-    deque<glm::vec3> normals_temp;
+    std::deque<glm::vec3> verticies_temp;
+    std::deque<glm::vec2> uvs_temp;
+    std::deque<glm::vec3> normals_temp;
 
-    FaceType faceType = Unclassified;
-    ifstream file(filename);
-    string line_type;
-
-    while (file >> line_type)
+    auto *file = fopen(filename, "r");
+    if (!file)
     {
-        if (line_type == "v")
+        return;
+    }
+
+    char firstOnLine, secondOnLine;
+    FaceType faceType = Unclassified;
+
+    while ((firstOnLine = fgetc(file)) != EOF && (secondOnLine = fgetc(file)) != EOF)
+    {
+        if (firstOnLine == 'v' && secondOnLine == ' ')
         {
             glm::vec3 vertex;
-            file >> vertex.x >> vertex.y >> vertex.z;
+            fscanf(file, "%f %f %f", &vertex.x, &vertex.y, &vertex.z);
             verticies_temp.emplace_back(vertex);
         }
-        else if (line_type == "vt")
+        else if (firstOnLine == 'v' && secondOnLine == 't')
         {
             glm::vec2 uv;
-            file >> uv.x >> uv.y;
+            fscanf(file, "%f %f", &uv.x, &uv.y);
             uvs_temp.emplace_back(uv);
         }
-        else if (line_type == "vn")
+        else if (firstOnLine == 'v' && secondOnLine == 'n')
         {
             glm::vec3 normal;
-            file >> normal.x >> normal.y >> normal.z;
+            fscanf(file, "%f %f %f", &normal.x, &normal.y, &normal.z);
             normals_temp.emplace_back(normal);
         }
-        else if (line_type == "f")
+        else if (firstOnLine == 'f')
         {
             if (faceType == Unclassified)
             {
-                string face;
-                getline(file, face);
-                // tellg and seekg are stupid, back up using this instead
-                for (auto c : face)
+                std::string face(64, '\0');
+                fgets(&face[0], 64, file);
+                int length = static_cast<int>(strlen(face.c_str()));
+                // back up to start of line
+                for (int i = length-1; i >= 0; --i)
                 {
-                    file.unget();
+                    ungetc(face[i], file);
                 }
                 faceType = identifyFace(face);
             }
-
-            verticies.reserve(verticies_temp.size());
-            uvcoords.reserve(uvs_temp.size());
-            normals.reserve(normals_temp.size());
 
             uint verts[3] = {};
             uint uvs[3] = {};
             uint norms[3] = {};
             if (faceType == PosUvNorm)
             {
-                file >> verts[0] >> "/" >> uvs[0] >> "/" >> norms[0]
-                     >> verts[1] >> "/" >> uvs[1] >> "/" >> norms[1]
-                     >> verts[2] >> "/" >> uvs[2] >> "/" >> norms[2];
+                fscanf(file, "%i/%i/%i %i/%i/%i %i/%i/%i",
+                       &verts[0], &uvs[0], &norms[0],
+                       &verts[1], &uvs[1], &norms[1],
+                       &verts[2], &uvs[2], &norms[2]);
             }
             else if (faceType == PosNorm)
             {
-                file >> verts[0] >> "//" >> norms[0]
-                     >> verts[1] >> "//" >> norms[1]
-                     >> verts[2] >> "//" >> norms[2];
+                fscanf(file, "%i//%i %i//%i %i//%i",
+                       &verts[0], &norms[0],
+                       &verts[1], &norms[1],
+                       &verts[2], &norms[2]);
             }
             else if (faceType == PosUv)
             {
-                file >> verts[0] >> "/" >> uvs[0]
-                     >> verts[1] >> "/" >> uvs[1]
-                     >> verts[2] >> "/" >> uvs[2];
+                fscanf(file, "%i/%i %i/%i %i/%i",
+                       &verts[0], &uvs[0],
+                       &verts[1], &uvs[1],
+                       &verts[2], &uvs[2]);
             }
             else // faceType == Pos
             {
-                file >> verts[0] >> verts[1] >> verts[2];
+                fscanf(file, "%i %i %i", &verts[0], &verts[1], &verts[2]);
             }
 
             if (verts[0] != 0)
             {
+                verticies.reserve(verticies_temp.size());
                 verticies.emplace_back(verticies_temp[verts[0] - 1]);
                 verticies.emplace_back(verticies_temp[verts[1] - 1]);
                 verticies.emplace_back(verticies_temp[verts[2] - 1]);
             }
             if (norms[0] != 0)
             {
+                normals.reserve(normals_temp.size());
                 normals.emplace_back(normals_temp[norms[0] - 1]);
                 normals.emplace_back(normals_temp[norms[1] - 1]);
                 normals.emplace_back(normals_temp[norms[2] - 1]);
             }
             if (uvs[0] != 0)
             {
+                uvcoords.reserve(uvs_temp.size());
                 uvcoords.emplace_back(uvs_temp[uvs[0] - 1]);
                 uvcoords.emplace_back(uvs_temp[uvs[1] - 1]);
                 uvcoords.emplace_back(uvs_temp[uvs[2] - 1]);
             }
         }
-        file.ignore(numeric_limits<streamsize>::max(), '\n');
+        SkipToNextLine(file);
     }
+    fclose(file);
 }
 
 Mesh LoadMeshFromOBJ(const char *filename)
