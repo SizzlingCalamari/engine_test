35ebd6154ad1856a8415c742177e5db2f87a2c66
Jordan Cristiano <jordan.cristi@gmail.com>
2014-07-20 22:57:44 -0400

    Added 3D texture class

--- src/game/Renderer/3DRenderer/Texture.cpp
+++ src/game/Renderer/3DRenderer/Texture.cpp
@@ -16,7 +16,7 @@ void Texture::LoadTexture2D(const uint8* pixels, ptrdiff_t stride, uint width, u
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
     const auto format = (stride == 3) ? GL_RGB : GL_RGBA;
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, format, GL_UNSIGNED_BYTE, pixels);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, format, GL_UNSIGNED_BYTE, pixels);
     glBindTexture(GL_TEXTURE_2D, 0);
 }
 
@@ -25,5 +25,40 @@ void Texture::FreeTexture()
     if (m_texture_2d != 0)
     {
         glDeleteTextures(1, &m_texture_2d);
+    }
+}
+
+void Texture3D::LoadTexture3D(const uint8* texels,
+                              GLenum format, GLenum type,
+                              uint width, uint height, uint depth)
+{
+    if (m_texture == 0)
+    {
+        glGenTextures(1, &m_texture);
+    }
+    assert(texels);
+    m_dimensions = glm::vec3(width, height, depth);
+
+    glBindTexture(GL_TEXTURE_3D, m_texture);
+
+    // linear filtering
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+    // repeated texture
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_REPEAT);
+
+    glTexImage3D(GL_TEXTURE_3D, 0, GL_RGBA, width, height, depth, 0, format, type, texels);
+
+    glBindTexture(GL_TEXTURE_3D, 0);
+}
+
+void Texture3D::FreeTexture()
+{
+    if (m_texture != 0)
+    {
+        glDeleteTextures(1, &m_texture);
     }
 }
--- src/game/Renderer/3DRenderer/Texture.h
+++ src/game/Renderer/3DRenderer/Texture.h
@@ -1,6 +1,9 @@
 
 #pragma once
 
+#include <glm/vec3.hpp>
+#include "GL/glew.h"
+
 class Texture
 {
 public:
@@ -34,3 +37,27 @@ inline uint Texture::GetGLId() const
 {
     return m_texture_2d;
 }
+
+class Texture3D
+{
+public:
+    void LoadTexture3D(const uint8* texels,
+                       GLenum format, GLenum type,
+                       uint width, uint height, uint depth);
+
+    void FreeTexture();
+
+    const glm::uvec3& GetDimensions() const
+    {
+        return m_dimensions;
+    }
+
+    uint GetGLId() const
+    {
+        return m_texture;
+    }
+    
+private:
+    uint m_texture = 0;
+    glm::uvec3 m_dimensions = glm::uvec3();
+};
