94801b97dbe221c88b544fd91324e8649152b4cf
Jordan Cristiano <jordan.cristi@gmail.com>
2016-05-14 22:56:37 -0400

    Fixed checkout revision for glm external

--- src/external/glm/commands.lua
+++ src/external/glm/commands.lua
@@ -1,6 +1,7 @@
 
 local repo = "https://github.com/g-truc/glm.git"
 local dir = path.getbasename(repo)
+local current_rev = "682979ddd8b4b0839c36bb0796a71c095b71cbd6"
 
 function getdir()
     return dir
@@ -10,6 +11,10 @@ function clone()
     if not os.isdir(getdir() .. "/.git") then
         os.execute("git clone " .. repo .. " " .. getdir())
     end
+    os.chdir(dir)
+    os.execute("git reset --hard")
+    os.execute("git fetch origin master")
+    os.execute("git checkout " .. current_rev)
 end
 
 function buildLinux()
