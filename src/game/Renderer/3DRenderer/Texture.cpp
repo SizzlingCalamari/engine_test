4bab2a0eb38e10112bf1845c5a0ec36761c1fc3b
Jordan Cristiano <jordan.cristi@gmail.com>
2016-11-22 23:49:36 -0500

    Playing around with anisotropic filtering on 2d textures

--- src/game/Renderer/3DRenderer/Texture.cpp
+++ src/game/Renderer/3DRenderer/Texture.cpp
@@ -12,8 +12,12 @@ void Texture::LoadTexture2D(const uint8* pixels, ptrdiff_t stride, uint width, u
     m_width = width;
     m_height = height;
     glBindTexture(GL_TEXTURE_2D, m_texture_2d);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+    float maxAniso;
+    glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &maxAniso);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, maxAniso);
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
