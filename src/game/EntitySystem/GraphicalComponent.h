0c3917eb493b8252597f1aef3c439128e748d945
Jordan Cristiano <jordan.cristi@gmail.com>
2018-06-25 00:33:39 -0400

    Cleaned a few includes

--- src/game/EntitySystem/GraphicalComponent.h
+++ src/game/EntitySystem/GraphicalComponent.h
@@ -1,8 +1,6 @@
 
 #pragma once
 
-#include <string>
-
 struct GraphicalComponent
 {
     GraphicalComponent():
--- src/game/EntitySystem/PhysicalComponent.h
+++ src/game/EntitySystem/PhysicalComponent.h
@@ -1,7 +1,7 @@
 
 #pragma once
 
-#include <glm/glm.hpp>
+#include <glm/vec3.hpp>
 #include <glm/gtc/quaternion.hpp>
 
 struct PhysicalComponent
