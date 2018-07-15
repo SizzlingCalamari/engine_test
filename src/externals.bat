c304fbdcc2ff18a754e372c5c2cd5c3493ffe75a
SizzlingCalamari <jordan.cristi@gmail.com>
2018-07-14 23:09:46 -0400

    Hack to try to find msbuild on AppVeyor.
    MSBuild doesn't exist in TestAgent installs, so the assumption that TestAgent/MSBuild exists is false.
    A separate BuildTools directory (if it was installed) alongside TestAgent should contain MSBuild in that case.
    
    For now, just try to use msbuild directly if it exists.

--- src/externals.bat
+++ src/externals.bat
@@ -1,5 +1,12 @@
 
 setlocal
+
+where msbuild >nul 2>nul
+if %ERRORLEVEL% == 0 (
+  set MSBUILD_PATH="msbuild"
+  GOTO Build
+)
+
 for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -property installationPath`) do (
   set InstallDir=%%i
 )
@@ -9,6 +16,8 @@ if exist "%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe" (
 ) ELSE (
   GOTO End
 )
+
+:Build
 
 premake5 --file=external/command_all.lua "clone"
 premake5 --file=external/command_all.lua "build"
